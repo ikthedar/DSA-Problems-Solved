920db9847e2ff238544272c3946dde5d60bdb196
Ikthedar Chowdhury <ikchy1994@gmail.com>
2022-09-18 23:40:12 +0600

    Update Valid Parentheses.py

--- Valid Parentheses.py
+++ Valid Parentheses.py
@@ -1,10 +1,13 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # Setting the stack as a list
         stack = []
+        # Always keep the closing parenthesis first for mapping to the opening one
         closeToOpen = {")":"(","}":"{","]":"["}
         
         for c in s:
             if c in closeToOpen:
+                # stack[-1] is the last added element of the stack
                 if stack and stack[-1] == closeToOpen[c]:
                     stack.pop()
                 else:
